4a33ef67371e5bddd25cb9e676f47697c9ebfa75
Tommy Trøen <tommy.troen@nav.no>
2020-05-25 13:13:36 +0200

    chore: remove hack for missing cluster in client_id

--- src/main/kotlin/io/nais/security/oauth2/registration/ClientRegistry.kt
+++ src/main/kotlin/io/nais/security/oauth2/registration/ClientRegistry.kt
@@ -1,7 +1,6 @@
 package io.nais.security.oauth2.registration
 
 import io.nais.security.oauth2.config.ClientRegistryProperties
-import io.nais.security.oauth2.model.AccessPolicy
 import io.nais.security.oauth2.model.ClientId
 import io.nais.security.oauth2.model.OAuth2Client
 import mu.KotlinLogging
@@ -15,34 +14,13 @@ open class ClientRegistry(
 
     fun findClient(clientId: ClientId): OAuth2Client? = clientStore.find(clientId)
 
-    // TODO: remove manual patch
     fun registerClient(client: OAuth2Client): OAuth2Client {
         log.info("register client with clientId=${client.clientId} and keyIds=${client.jwkSet.keys.map { it.keyID }.toList()}")
-        val updatePolicyIfNeccessary = handleMissingClusterInClientId(client)
-        clientStore.storeClient(updatePolicyIfNeccessary)
+        clientStore.storeClient(client)
         return client
     }
 
     fun findAll(): List<OAuth2Client> = clientStore.findAll()
 
     fun deleteClient(clientId: ClientId) = clientStore.delete(clientId)
-}
-
-fun ClientRegistry.handleMissingClusterInClientId(client: OAuth2Client): OAuth2Client {
-    return when{
-        client.clientId.startsWith(":") -> {
-            val inboundPolicy = client.accessPolicyInbound.clients.map {
-                if(it.startsWith(":")) it else ":${it.substringAfter(":")}"
-            }
-            OAuth2Client(
-                client.clientId,
-                client.jwks,
-                AccessPolicy(inboundPolicy),
-                client.accessPolicyOutbound,
-                client.allowedScopes,
-                client.allowedGrantTypes
-            )
-        }
-        else -> client
-    }
 }
