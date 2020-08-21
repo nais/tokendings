ae0611c3fc28f17edf7d605ded80e7978b87291e
yousher <youssef.bel.mekki@nav.no>
2020-08-21 09:37:57 +0200

    Fix test. Could be solved differently

--- src/test/kotlin/io/nais/security/oauth2/rsakeystore/KeyStoreTest.kt
+++ src/test/kotlin/io/nais/security/oauth2/rsakeystore/KeyStoreTest.kt
@@ -5,6 +5,7 @@ import io.kotest.matchers.shouldNotBe
 import io.nais.security.oauth2.mock.DataSource
 import io.nais.security.oauth2.mock.withMigratedDb
 import org.junit.jupiter.api.Test
+import java.lang.Thread.sleep
 import java.time.LocalDateTime
 
 class KeyStoreTest {
@@ -29,8 +30,10 @@ class KeyStoreTest {
             with(KeyStore(DataSource.instance)) {
                 TTL = 1
                 val rsaKeysInitial = keys()
-                rsaKeysInitial.expired(LocalDateTime.now()) shouldBe false
-                rsaKeysInitial.expired(LocalDateTime.now().plusSeconds(2)) shouldBe true
+                val now = LocalDateTime.now()
+                rsaKeysInitial.expired(now) shouldBe false
+                rsaKeysInitial.expired(now.plusSeconds(2)) shouldBe true
+                sleepy(1)
                 val rsaKeysRotated = keys()
                 rsaKeysRotated.expired(LocalDateTime.now()) shouldBe false
                 rsaKeysInitial.nextKey shouldBe rsaKeysRotated.currentKey
@@ -40,5 +43,12 @@ class KeyStoreTest {
                 rsaKeysInitial.previousKey?.toRSAKey()?.isPrivate shouldBe true
             }
         }
+    }
+}
+
+internal fun sleepy(seconds: Int) {
+    try {
+        sleep(seconds * 1000.toLong())
+    } catch (e: InterruptedException) {
     }
 }
