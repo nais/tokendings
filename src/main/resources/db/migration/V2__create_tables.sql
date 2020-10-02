ad31dd1e16d5e59d23490d7182125a317e80e4d4
yousher <youssef.bel.mekki@nav.no>
2020-10-02 11:59:56 +0200

    fix: reformat of code in intellj make changes in files no changes is wanted.

--- src/main/resources/db/migration/V2__create_tables.sql
+++ src/main/resources/db/migration/V2__create_tables.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS token_issuer_keys;
 
 CREATE TABLE IF NOT EXISTS rotatable_keys
 (
-    id           BIGINT,
-    current_key  text        NOT NULL,
-    previous_key text        NOT NULL,
-    next_key     text        NOT NULL,
-    expiry       varchar(50) NOT NULL,
+    id          BIGINT,
+    current_key  text            NOT NULL,
+    previous_key text            NOT NULL,
+    next_key     text            NOT NULL,
+    expiry       varchar(50)     NOT NULL,
     PRIMARY KEY (id)
 );
 
