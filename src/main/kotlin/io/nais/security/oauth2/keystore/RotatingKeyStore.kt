9b09f81286a79c84bf316f54d33a08d467cb0c71
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-10-10 16:38:28 +0200

    refactor: perform extra read before upsert; extract functions for clarity; naming

--- src/main/kotlin/io/nais/security/oauth2/keystore/RotatingKeyStore.kt
+++ src/main/kotlin/io/nais/security/oauth2/keystore/RotatingKeyStore.kt
@@ -7,61 +7,69 @@ import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jose.jwk.source.JWKSource
 import com.nimbusds.jose.proc.SecurityContext
 import io.nais.security.oauth2.config.KeyStoreProperties
-import io.nais.security.oauth2.utils.generateRsaKey
 import mu.KotlinLogging
 import org.slf4j.Logger
 import java.time.Duration
-import java.time.LocalDateTime
 
 private val log: Logger = KotlinLogging.logger { }
 
 class RotatingKeyStore(keyStoreProperties: KeyStoreProperties) : JWKSource<SecurityContext?> {
     private val keyStore: KeyStore = KeyStore(keyStoreProperties.dataSource)
     private val rotationInterval: Duration = keyStoreProperties.rotationInterval
-    private var rotatableKeys: RotatableKeys = keyStore.read() ?: saveGeneratedRsaKeys()
+    private var rotatableKeys: RotatableKeys = getOrGenerateKeys()
 
     fun currentSigningKey(): RSAKey {
-        return getAndRotateKeys(rotationInterval).currentKey
+        return getAndRotateKeysIfExpired().currentKey
     }
 
     val publicJWKSet: JWKSet
         get() {
-            val keys = getAndRotateKeys(rotationInterval)
+            val keys: RotatableKeys = getAndRotateKeysIfExpired()
             val jwkList: MutableList<JWK> = ArrayList()
             jwkList.add(keys.currentKey)
             jwkList.add(keys.previousKey)
             return JWKSet(jwkList).toPublicJWKSet()
         }
 
-    private fun getAndRotateKeys(rotationInterval: Duration): RotatableKeys {
-        log.debug("check keys for expiry and rotate if neccessary")
+    private fun getAndRotateKeysIfExpired(): RotatableKeys {
+        log.debug("checking keys for expiry and rotating if necessary")
         if (rotatableKeys.expired()) {
-            keyStore.read()?.let { current ->
-                rotatableKeys = current
-                if (current.expired()) {
-                    val expiry = LocalDateTime.now().plus(rotationInterval)
-                    rotatableKeys = current.rotate(expiry).also {
-                        keyStore.save(it)
-                        log.info("Keys rotated, next expiry: $expiry")
-                    }
-                }
-            } ?: throw RuntimeException("Could not get current keys from storage")
+            rotatableKeys = getOrGenerateKeys()
         }
         return rotatableKeys
+    }
+
+    private fun getOrGenerateKeys(): RotatableKeys {
+        val keys: RotatableKeys = keyStore.read() ?: generateKeysAndSave()
+        if (keys.notExpired()) {
+            return keys
+        }
+        return rotateKeysAndSave(keys)
+    }
+
+    private fun generateKeysAndSave(): RotatableKeys =
+        RotatableKeys
+            .generate(expiresIn = rotationInterval)
+            .saveToKeyStore()
+            .also { log.info("No previous key set found. Initialised new key set, next expiry: ${it.expiry}") }
+
+    private fun rotateKeysAndSave(keys: RotatableKeys): RotatableKeys =
+        keys.rotate(expiresIn = rotationInterval)
+            .saveToKeyStore()
+            .also { log.info("Keys rotated, next expiry: ${it.expiry}") }
+
+    private fun RotatableKeys.saveToKeyStore(): RotatableKeys {
+        // lookup keys one last time before upserting
+        keyStore.read()?.let { keys ->
+            if (keys.notExpired()) {
+                return keys
+            }
+        }
+        keyStore.save(this)
+        return this
     }
 
     override fun get(jwkSelector: JWKSelector?, context: SecurityContext?): List<JWK> {
         return publicJWKSet.keys
     }
-
-    private fun saveGeneratedRsaKeys(): RotatableKeys =
-        RotatableKeys(
-            currentKey = generateRsaKey(),
-            previousKey = generateRsaKey(),
-            nextKey = generateRsaKey(),
-            expiry = LocalDateTime.now().plus(rotationInterval)
-        ).also {
-            keyStore.save(it)
-            log.info("RSA KEY initialised, next expiry: ${it.expiry}")
-        }
 }
