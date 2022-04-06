353e4b9ef1d36de336248e3fc03d40d4f8b616b0
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2022-04-06 14:32:21 +0200

    fix: actually use default resource retriever timeouts instead of infinite

--- src/main/kotlin/io/nais/security/oauth2/model/CacheProperties.kt
+++ src/main/kotlin/io/nais/security/oauth2/model/CacheProperties.kt
@@ -1,6 +1,9 @@
 package io.nais.security.oauth2.model
 
 import com.nimbusds.jose.jwk.source.DefaultJWKSetCache
+import com.nimbusds.jose.jwk.source.RemoteJWKSet.DEFAULT_HTTP_CONNECT_TIMEOUT
+import com.nimbusds.jose.jwk.source.RemoteJWKSet.DEFAULT_HTTP_READ_TIMEOUT
+import com.nimbusds.jose.jwk.source.RemoteJWKSet.DEFAULT_HTTP_SIZE_LIMIT
 import com.nimbusds.jose.util.DefaultResourceRetriever
 import io.nais.security.oauth2.token.FailoverJwks
 import java.net.URL
@@ -11,6 +14,9 @@ data class CacheProperties(
     val refreshTime: Long,
     val timeUnit: TimeUnit,
     val jwksURL: URL,
+    val connectionTimeout: Int = DEFAULT_HTTP_CONNECT_TIMEOUT,
+    val readTimeOut: Int = DEFAULT_HTTP_READ_TIMEOUT,
+    val sizeLimit: Int = DEFAULT_HTTP_SIZE_LIMIT
 ) {
     val configurableJWKSetCache = DefaultJWKSetCache(
         this.lifeSpan,
@@ -18,7 +24,7 @@ data class CacheProperties(
         this.timeUnit
     )
 
-    val configurableResourceRetriever = DefaultResourceRetriever()
+    val configurableResourceRetriever = DefaultResourceRetriever(connectionTimeout, readTimeOut, sizeLimit)
 
     val failoverJwks = FailoverJwks(
         this.jwksURL,
