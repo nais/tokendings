ae6ffd8f540a402e4b0b2594f78d81808aa6fe67
J-K. Solbakken <jksolbakken@gmail.com>
2023-11-13 14:53:38 +0100

    adhere to changes in the nimbus caching api

--- src/main/kotlin/io/nais/security/oauth2/token/TokenValidator.kt
+++ src/main/kotlin/io/nais/security/oauth2/token/TokenValidator.kt
@@ -2,24 +2,17 @@ package io.nais.security.oauth2.token
 
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.jwk.source.JWKSource
-import com.nimbusds.jose.jwk.source.RemoteJWKSet
 import com.nimbusds.jose.proc.JWSVerificationKeySelector
 import com.nimbusds.jose.proc.SecurityContext
 import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.SignedJWT
 import io.nais.security.oauth2.model.CacheProperties
-import java.net.URL
 
-class TokenValidator(private val issuer: String, jwkSource: JWKSource<SecurityContext?>) {
+class TokenValidator(private val issuer: String, jwkSource: JWKSource<SecurityContext>) {
 
-    constructor(issuer: String, jwkSetUri: URL, cacheProperties: CacheProperties) : this(
+    constructor(issuer: String, cacheProperties: CacheProperties) : this(
         issuer,
-        RemoteJWKSet(
-            jwkSetUri,
-            cacheProperties.failoverJwks,
-            cacheProperties.configurableResourceRetriever,
-            cacheProperties.configurableJWKSetCache
-        )
+        cacheProperties.jwkSource
     )
 
     private val keySelector = JWSVerificationKeySelector(JWSAlgorithm.RS256, jwkSource)
