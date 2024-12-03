76c22618e644b47098ffd50b08c582f764ec4bda
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2024-12-03 15:43:36 +0100

    add(database-config): prod and dev
    * clean up charts
    * apiserver, only jwker
    * tokendings enabled when switched on for feature
    
    * add missing replication config for tokendings host.

--- src/main/kotlin/io/nais/security/oauth2/config/DatabaseConfig.kt
+++ src/main/kotlin/io/nais/security/oauth2/config/DatabaseConfig.kt
@@ -3,12 +3,16 @@ package io.nais.security.oauth2.config
 import com.codahale.metrics.MetricRegistry
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import io.nais.security.oauth2.config.HikariProperties.CONNECTION_TIMEOUT
-import io.nais.security.oauth2.config.HikariProperties.IDLE_TIMEOUT
-import io.nais.security.oauth2.config.HikariProperties.INITIALIZATION_FAIL_TIMEOUT
-import io.nais.security.oauth2.config.HikariProperties.MAX_LIFETIME
-import io.nais.security.oauth2.config.HikariProperties.MAX_POOL_SIZE
-import io.nais.security.oauth2.config.HikariProperties.MIN_IDLE_CONNECTIONS
+import io.nais.security.oauth2.config.HikariProperties.CONNECTION_TIMEOUT_NON_PROD
+import io.nais.security.oauth2.config.HikariProperties.CONNECTION_TIMEOUT_PROD
+import io.nais.security.oauth2.config.HikariProperties.IDLE_TIMEOUT_NON_PROD
+import io.nais.security.oauth2.config.HikariProperties.IDLE_TIMEOUT_PROD
+import io.nais.security.oauth2.config.HikariProperties.MAX_LIFETIME_NON_PROD
+import io.nais.security.oauth2.config.HikariProperties.MAX_LIFETIME_PROD
+import io.nais.security.oauth2.config.HikariProperties.MAX_POOL_SIZE_NON_PROD
+import io.nais.security.oauth2.config.HikariProperties.MAX_POOL_SIZE_PROD
+import io.nais.security.oauth2.config.HikariProperties.MIN_IDLE_CONNECTIONS_NON_PROD
+import io.nais.security.oauth2.config.HikariProperties.MIN_IDLE_CONNECTIONS_PROD
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.dropwizard.DropwizardExports
 import org.flywaydb.core.Flyway
@@ -32,22 +36,40 @@ internal fun clean(dataSource: HikariDataSource) =
 private fun hikariConfig(databaseConfig: DatabaseConfig) =
     HikariConfig().apply {
         jdbcUrl = databaseConfig.url
-        maximumPoolSize = MAX_POOL_SIZE
-        minimumIdle = MIN_IDLE_CONNECTIONS
-        idleTimeout = IDLE_TIMEOUT
-        connectionTimeout = CONNECTION_TIMEOUT
-        maxLifetime = MAX_LIFETIME
-        initializationFailTimeout = INITIALIZATION_FAIL_TIMEOUT
+        if (isNonProd()) {
+            maximumPoolSize = MAX_POOL_SIZE_NON_PROD
+            minimumIdle = MIN_IDLE_CONNECTIONS_NON_PROD
+            idleTimeout = IDLE_TIMEOUT_NON_PROD
+            connectionTimeout = CONNECTION_TIMEOUT_NON_PROD
+            maxLifetime = MAX_LIFETIME_NON_PROD
+        } else {
+            maximumPoolSize = MAX_POOL_SIZE_PROD
+            minimumIdle = MIN_IDLE_CONNECTIONS_PROD
+            idleTimeout = IDLE_TIMEOUT_PROD
+            connectionTimeout = CONNECTION_TIMEOUT_PROD
+            maxLifetime = MAX_LIFETIME_PROD
+        }
+        initializationFailTimeout = HikariProperties.INITIALIZATION_FAIL_TIMEOUT
         metricRegistry = databaseConfig.metricRegistry
     }.also {
         CollectorRegistry.defaultRegistry.register(DropwizardExports(databaseConfig.metricRegistry))
     }
 
 object HikariProperties {
-    const val IDLE_TIMEOUT = 10001L
-    const val CONNECTION_TIMEOUT = 3000L
-    const val MAX_LIFETIME = 30001L
-    const val MAX_POOL_SIZE = 10
-    const val MIN_IDLE_CONNECTIONS = 5
+    // Production-specific
+    const val IDLE_TIMEOUT_PROD = 300000L
+    const val CONNECTION_TIMEOUT_PROD = 5000L
+    const val MAX_LIFETIME_PROD = 1800000L
+    const val MAX_POOL_SIZE_PROD = 10
+    const val MIN_IDLE_CONNECTIONS_PROD = 5
+
+    // Non-production-specific
+    const val IDLE_TIMEOUT_NON_PROD = 600000L
+    const val CONNECTION_TIMEOUT_NON_PROD = 10000L
+    const val MAX_LIFETIME_NON_PROD = 3600000L
+    const val MAX_POOL_SIZE_NON_PROD = 10
+    const val MIN_IDLE_CONNECTIONS_NON_PROD = 5
+
+    // Common configuration
     const val INITIALIZATION_FAIL_TIMEOUT = 10000L
 }
